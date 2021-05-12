f381de826f858d4edded59f607880a4bcb8b5917
GraceNam <zeidgame@gmail.com>
2021-05-13 01:17:26 +0900

    feature : 몽고DB 연결
    + 노드 리액트 기초
      + mongoose 설치
      + 몽고 DB 연결

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express') // express module 가져옴
 const app = express() // function으로 앱을 만듬
 const port = 5000 // 백서버 port 번호
 
+// mongoDB connect
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://gracenam:abcd1234@basic-node.olyns.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))  
+
 // 루트에서 hello world를 출력
 app.get('/', (req, res) => {
   res.send('Hello World!')
